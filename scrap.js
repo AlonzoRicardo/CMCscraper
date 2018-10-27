a42da9f3d16100e7ccbd644dc75462fe23409ca7
RicardoAlonzo <alonzoricardo6@gmail.com>
2018-10-27 16:47:28 +0200

    creates csv files with rows and columns in the written folder

--- scrap.js
+++ scrap.js
@@ -1,5 +1,23 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
+const fs = require('fs');
+const ws = fs.createWriteStream('written/cmc.csv');
+
+ws.write(`rank, ticker, name, marketCap, price, volume24h, circulatingSupply, ticker2, change24h \n`)
+
+class Coin {
+    constructor(rank, ticker, name, marketCap, price, volume24h, circulatingSupply, ticker2, change24h) {
+        this.rank = rank;
+        this.ticker = ticker;
+        this.name = name;
+        this.marketCap = marketCap;
+        this.price = price;
+        this.volume24h = volume24h;
+        this.circulatingSupply = circulatingSupply;
+        this.ticker2 = ticker2;
+        this.change24h = change24h;
+    }
+}
 
 axios.request('https://coinmarketcap.com/')
     .then(res => {
@@ -7,20 +25,7 @@ axios.request('https://coinmarketcap.com/')
             normalizeWhitespace: true,
             xmlMode: true
         })
-
-        class Coin {
-            constructor(rank, ticker, name, marketCap, price, volume24h, circulatingSupply, ticker2, change24h) {
-                this.rank = rank;
-                this.ticker = ticker;
-                this.name = name;
-                this.marketCap = marketCap;
-                this.price = price;
-                this.volume24h = volume24h;
-                this.circulatingSupply = circulatingSupply;
-                this.ticker2 = ticker2;
-                this.change24h = change24h;
-            }
-        }
+        
 
         const table = $('tbody')
         table.find('ul').remove();
@@ -31,6 +36,7 @@ axios.request('https://coinmarketcap.com/')
         let spliced = [];
         let count = 2;
         let acc = [];
+
         for (let i = 0; i < outSplit.length; i++) {
             if (outSplit[i] == count) {
                 spliced.push(acc);
@@ -40,7 +46,15 @@ axios.request('https://coinmarketcap.com/')
             acc.push(outSplit[i]);
         }
 
+        let grouped = spliced.map(e => {
+            return new Coin( ...e )
+        })
+        
+        grouped.map(e => {
+            const {rank, ticker, name, marketCap, price, volume24h, circulatingSupply, ticker2, change24h} = e;
 
-        console.log(new Coin( ...spliced[0] ));
+            ws.write(` ${rank}, ${ticker}, ${name}, ${marketCap}, ${price}, ${volume24h}, ${circulatingSupply}, ${ticker2}, ${change24h} \n`)
+        })
+
 
     })
